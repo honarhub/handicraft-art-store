27e9b9f1f988b7a733d7d212deeba225863f5cf4
Abbas Ranjbar <ranjbar.abbas1988@gmail.com>
2026-09-21 16:36:09 +0100

    Disable standalone output for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // output: "standalone", // Vercel با این حالت تداخل دارد. برای داکر/هتزنر از کامنت خارج کنید
 };
 
 export default nextConfig;
